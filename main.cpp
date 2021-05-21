17e5d1844f2deeec34cb06a5b6c05d75fa6886ea
unknown <luiscf1226@gmail.com>
2021-05-21 12:19:34 -0600

    creacion

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+int menu(){
+
+	
+	while(true){
+		cout<<"MENU"<<endl;
+		cout<<"1. Ejercicio 1 "<<endl<<"2. Ejercicio 2 "<<endl<<"3. Ejercicio 3"<<endl<<"4. Salir"<<endl;
+		int valor;
+		cin>>valor;
+		if(valor > 0&& valor < 5)
+			return valor;
+		
+	}
+	
+}*/
+int main(int argc, char** argv) {
+	/*
+	int main=0;
+	while(main!=4){
+		switch(main=menu()){
+			case 1:{
+			
+			}
+			case 2:{
+			
+			}
+			case 3:{
+			
+			}
+			case 4:{
+				cout<<"el programa se acabara "<<endl;
+				
+				break;
+			}
+		}//sitwch
+	}//while*/
+	
+	return 0;
+}
